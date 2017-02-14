db190be760a96b7e7c436b1cbd51d3ceadb84b59
QuentinSup <qsupernant@gmail.com>
2017-02-14 21:58:30 +0100

    Add template for new project

---
+++ www/template/controllers/main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace babylifeshare;
+
+use dw\dwFramework as dw;
+use dw\classes\dwHttpRequest;
+use dw\classes\dwHttpResponse;
+use dw\classes\dwModel;
+use dw\classes\dwObject;
+use dw\classes\controllers\dwBasicController;
+use dw\accessors\ary;
+
+/**
+ * @Mapping(value = '/')
+ */
+class main extends dwBasicController {
+
+	/**
+	 * @Mapping(method = "get")
+	 */
+	public function root(dwHttpRequest &$request, dwHttpResponse &$response, dwModel &$model) 
+	{				
+		return 'Hello world !';
+	}
+
+}
+
+?>
