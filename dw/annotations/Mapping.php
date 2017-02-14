1459ffd734f0d2db9f68dd037b559e0aa95f5d6b
QuentinSup <qsupernant@gmail.com>
2017-02-14 21:48:24 +0100

    Add comments

--- dw/annotations/Mapping.php
+++ dw/annotations/Mapping.php
@@ -1,23 +1,39 @@
 <?php
 
+/**
+ * Annotation Mapping
+ * @author qsupernant
+ */
 class Mapping extends Annotation {
 	
 	protected $method = null;
 	protected $consumes = null;
 	protected $produces = null;
 	
+	/**
+	 * @return the value set
+	 */
 	public function getValue() {
 		return $this -> value;
 	}
 	
+	/**
+	 * @return the method set
+	 */
 	public function getMethod() {
 		return $this -> method;
 	}
 	
+	/**
+	 * @return the consumes property value
+	 */
 	public function getConsumes() {
 		return $this -> consumes;
 	}
 	
+	/**
+	 * @return the produces property value
+	 */
 	public function getProduces() {
 		return $this -> produces;
 	}
