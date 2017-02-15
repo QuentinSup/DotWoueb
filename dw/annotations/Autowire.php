3f51c80d14cbab27f20bef78ccf3b5e64b6dab9a
QuentinSup <qsupernant@gmail.com>
2017-02-15 16:15:49 +0100

    New Autowire annotation

---
+++ dw/annotations/Autowire.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Annotation Autowire
+ * @author QuentinSup
+ */
+class Autowire extends Annotation {
+	
+	protected $name = '';
+	
+	/**
+	 * @return the value set
+	 */
+	public function getValue() {
+		return $this -> value;
+	}
+	
+	/**
+	 * @return the name set
+	 */
+	public function getName() {
+		return $this -> name;
+	}
+	
+	/**
+	 * 
+	 * @param unknown $class
+	 */
+	public static function processProperty($app, $reflection, $reflectionProperty, $annotationClass, $annotationProperty) {
+		
+		if($annotationProperty) {
+		
+			if($annotationProperty -> getValue()) {
+				
+				
+				$className = $reflection -> name;
+				$propName = $reflectionProperty -> name;
+				
+				// Logger
+				if($annotationProperty -> getValue() == 'logger') {
+					eval("$className::\$$propName = dw\\classes\\dwLogger::getLogger('$className');");
+					return;
+				}
+				
+				if($annotationProperty -> getValue() == 'connector') {
+					$connectorName = $annotationProperty -> getName();
+					eval("$className::\$$propName =  dw\dwFramework::App ()->getConnector ( '$connectorName' );");
+				}
+
+			}
+				
+			
+		}
+	}
+	
+	
+}
+
+?>
