d7dd3be2205799087e954116f47a7c75d3f200ce
QuentinSup <qsupernant@gmail.com>
2017-02-20 10:07:04 +0100

    Add comments

--- dw/dwConnectors.php
+++ dw/dwConnectors.php
@@ -5,10 +5,19 @@ namespace dw;
 use dw\helpers\dwFile;
 use dw\accessors\ary;
 
+/**
+ * Manage connector list
+ * @author QuentinSup
+ * @package dw
+ */
 class dwConnectors {
 	
 	protected static $_connectors = array();
 	
+	/**
+	 * Load a connector files from directory
+	 * @param $dirpath the directory to found the connectors
+	 */
 	public static function loadConnectors($dirpath) {
 		
 		$connectorsList = dwFile::ls($dirpath);
@@ -27,7 +36,11 @@ class dwConnectors {
 		}
 
 	}
-	
+	/**
+	 * Return a new instance of a connector
+	 * @param $name the connector name to build
+	 * @return the instance|NULL
+	 */
 	public static function factory($name) {
 		$connectorClassName = ary::get(self::$_connectors, $name, null);
 		return $connectorClassName?new $connectorClassName():null;
