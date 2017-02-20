33eb9fc40bc1d5afd210b88147a03311c19bf17d
QuentinSup <qsupernant@gmail.com>
2017-02-20 10:13:32 +0100

    Add comments

--- dw/dwInterceptors.php
+++ dw/dwInterceptors.php
@@ -12,7 +12,9 @@ use dw\classes\dwModel;
 define('E_LISTENER_UNKNOW', 101);
 
 /**
- * @author Quentin Supernant
+ * Manager interceptors
+ * Interceptor are classes which intercept HTTP request before and after being threated by a controller
+ * @author QuentinSup
  * @version 1.0
  * @package dotWoueb
  */
@@ -31,11 +33,22 @@ class dwInterceptors {
 		return $logger;
 	}
 	
+	/**
+	 * Returne all interceptors list
+	 * @return unknown
+	 */
 	public static function getInterceptors()
 	{
 		return self::$_aLoaded;	
 	}
 	
+	/**
+	 * Return a load interceptor
+	 * Throw an exception is the interceptor is not loaded
+	 * @param $sname name of the interceptor
+	 * @throws dwException
+	 * @return unknown
+	 */
 	public static function getInterceptor($sname)
 	{
 		if(!self::isLoaded($sname))
@@ -45,11 +58,25 @@ class dwInterceptors {
 		return self::$_aLoaded[$sname];
 	}
 	
+	/**
+	 * Return true if the interceptor has been loaded
+	 * @param $sname name of the interceptor
+	 * @return unknown
+	 */
 	public static function isLoaded($sname)
 	{
 		return isset(self::$_aLoaded[$sname]);
 	}
 	
+	/**
+	 * Load an interceptor by his name
+	 * The PHP filename must exist with this name into the path directory
+	 * @param $sname name of the interceptor
+	 * @param string $path The path to find the PHP file
+	 * @param boolean $useCache
+	 * @throws dwException
+	 * @return unknown
+	 */
 	public static function &loadInterceptor($sname, $path = APP_INTERCEPTORS_DIR, $useCache = true)
 	{
 		if(is_null($path)) {
@@ -61,13 +88,18 @@ class dwInterceptors {
 				self::logger() -> trace("Load interceptor file '$path.$sname.php'");
 			}
 			
+			// include php file
 			include_once($path.$sname.'.php');
+			
+			// try to find the className into file
 			$class = dw::App() -> getNamespace().$sname.self::$suffixClassName;
 			
 			if(!class_exists($class))
 			{
 				throw new dwException("Class '$class' for interceptor '$sname' doesn't exist");	
 			} else {
+				
+				// Create an instance and check heritage
 				$listener = new $class($sname);
 				if(!is_subclass_of($listener, "dw\classes\dwInterceptorInterface")) {
 					throw new dwException("Class '$class' for interceptor '$sname' is not a subclass of dw\classes\dwInterceptorInterface");	
@@ -80,6 +112,13 @@ class dwInterceptors {
 		return self::$_aLoaded[$sname];	
 	}
 	 
+	/**
+	 * Load one or multiple interceptors
+	 * @param mixed $interceptors could be the name of an interceptor to load, or a list of names
+	 * @param string $path
+	 * @param boolean $useCache
+	 * @return unknown
+	 */
 	public static function &load($interceptors, $path = APP_INTERCEPTORS_DIR, $useCache = true)
 	{
 		if(is_array($interceptors))
@@ -93,6 +132,14 @@ class dwInterceptors {
 		}
 	}
 	
+	/**
+	 * Do a function for all interceptors loaded
+	 * @param unknown $sfunction
+	 * @param dwHttpRequest $request
+	 * @param dwHttpResponse $response
+	 * @param dwModel $model
+	 * @return boolean
+	 */
 	public static function forAllInterceptorsDo($sfunction, dwHttpRequest $request, dwHttpResponse $response, dwModel $model)
 	{
 		foreach(self::$_aLoaded as $interceptor)
