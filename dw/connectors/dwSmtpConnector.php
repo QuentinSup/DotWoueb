0ea4995b9575224dce92c45b7a15b1d6200499b4
QuentinSup <qsupernant@gmail.com>
2017-03-06 14:38:31 +0100

    Integrate PHPMailer

--- dw/connectors/dwSmtpConnector.php
+++ dw/connectors/dwSmtpConnector.php
@@ -4,6 +4,8 @@ namespace dw\connectors;
 
 use dw\classes\dwConnectorInterface;
 
+dw_require('vendors/PHPMailer/class.phpmailer');
+
 /**
  * Smtp connector
  * @author QuentinSup
@@ -82,18 +84,47 @@ class dwSmtpConnector implements dwConnectorInterface {
 		ini_set("SMTP", $this -> _smtp);
 		ini_set("smtp_port", $this -> _port);
 
+		$mail = new \PHPMailer();
+		
+		$mail->Host = $this -> _smtp;  							// Specify main and backup SMTP servers
+		$mail->Port = $this -> _port;                              // TCP port to connect to
+		$mail->CharSet = "utf-8";
+		$mail->Encoding = "base64";
+		
+		$mail->setFrom($this -> _from);
+		$mail->addAddress($this -> fMailField($to));     // Add a recipient
+		
+		//$mail->addReplyTo('info@example.com', 'Information');
+		$mail->addCC($this -> fMailField($cc));
+		$mail->addBCC($this -> fMailField($bcc));
+		
+		//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+		//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+		$mail->isHTML(true);                                  // Set email format to HTML
+		
+		$mail->Subject = $subject;
+		$mail->Body    = $text;
+		//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+		
+
+		/*
 		// Always set content-type when sending HTML email
-		$headers = "MIME-Version: 1.0"."\r\n";
-		$headers .= "Content-type:text/html;charset=UTF-8"."\r\n";
-		$headers .= 'From: '.$this -> _from."\r\n";
+		$headers = "MIME-Version: 1.0"."\n";
+		$headers .= "Content-Type: text/html; charset=\"UTF-8\""."\n";
+		$headers .= 'From: '.$this -> _from."\n";
 		if(isset($cc)) {
-			$headers .= 'Cc: '.$this -> fMailField($cc)."\r\n";	
+			$headers .= 'Cc: '.$this -> fMailField($cc)."\n";	
 		}
 		if(isset($bcc)) {
-			$headers .= 'Bcc: '.$this -> fMailField($bcc)."\r\n";
+			$headers .= 'Bcc: '.$this -> fMailField($bcc)."\n";
 		}
-
-		return mail($this -> fMailField($to), $subject, $text, $headers);
+		*/
+		
+		//return mail($this -> fMailField($to), "=?UTF-8?B?".base64_encode($subject)."?=", $text, $headers);
+		//return mail(null, "=?UTF-8?B?".base64_encode($subject)."?=", null, $headers);
+		
+		return $mail -> send();
+		
 	}
 	
 }
