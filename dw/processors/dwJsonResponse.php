91be7af05b17824744409bf055e550496de67966
Quentin Supernant <qsupernant@clevertech-it.fr>
2020-09-13 15:58:16 +0200

    Corrige problème de renvoi JSON avec utf8 incorrect

--- dw/processors/dwJsonResponse.php
+++ dw/processors/dwJsonResponse.php
@@ -14,7 +14,7 @@ class dwJsonResponse extends dwTextResponse {
 	    
 	    if(isset($data) && (is_array($data) || strlen($data) > 0)) {
 	        
-		      $json = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+	        $json = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_INVALID_UTF8_IGNORE);
 		
     		  if($json === FALSE) {
                 throw new \Exception(json_last_error_msg());
